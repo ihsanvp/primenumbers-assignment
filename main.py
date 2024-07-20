435778c7b4c599e589c5ebe4d893a7697cc5b1f0
ihsanvp <ihsanvp2738@gmail.com>
2024-07-20 08:06:48 +0530

    fix fetching incorrect data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 import json
 
 def init_driver() -> webdriver.Chrome:
@@ -14,10 +15,11 @@ def init_driver() -> webdriver.Chrome:
 
 def get_project_data(driver, target_button, close_button):
     target_button.click()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'modal-data-display-tab_project_main-content')))
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'project-menu-html')))
     WebDriverWait(driver, 10).until(EC.visibility_of_any_elements_located((By.XPATH, "//tr[td[text()='PAN No.']]/td[2]/span")))
+    sleep(0.5)
 
-    parent = driver.find_element(By.ID, 'modal-data-display-tab_project_main-content')
+    parent = driver.find_element(By.ID, 'project-menu-html')
     details = {
         'gstin_no': parent.find_element(By.XPATH, "//tr[td[text()='GSTIN No.']]/td[2]/span").text,
         'pan_no': parent.find_element(By.XPATH, "//tr[td[text()='PAN No.']]/td[2]/span").text,
@@ -26,7 +28,7 @@ def get_project_data(driver, target_button, close_button):
     }
 
     close_button.click()
-    WebDriverWait(driver, 10).until(EC.invisibility_of_element((By.ID, 'modal-data-display-tab_project_main-content')))
+    WebDriverWait(driver, 10).until(EC.invisibility_of_element((By.ID, 'project-menu-html')))
     return details
 
 
@@ -39,9 +41,8 @@ def scrape_projects(limit = None):
     driver.get(url)
 
     print("waiting for data...")
-    WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.ID, 'reg-Projects'))
-    )
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, 'reg-Projects')))
+    sleep(1)
 
     projects = []
     project_detail_buttons = driver.find_elements(By.CSS_SELECTOR, 'a[title="View Application"]')
